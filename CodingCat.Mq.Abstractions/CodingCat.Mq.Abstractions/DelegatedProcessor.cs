a5592fef958642b448ac9b8fd346ababb0e15751
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-02-28 21:40:36 +0800

    implemented the abstract `DelegatedProcessor`

---
+++ CodingCat.Mq.Abstractions/CodingCat.Mq.Abstractions/DelegatedProcessor.cs
@@ -0,0 +1,41 @@
+﻿namespace CodingCat.Mq.Abstractions
+{
+    public abstract class DelegatedProcessor<T> : Processor<T>
+    {
+        public delegate void ProcessDelegate(T input);
+
+        public ProcessDelegate DelegatedProcess { get; }
+
+        #region Constructor(s)
+
+        public DelegatedProcessor(ProcessDelegate delegatedProcess)
+        {
+            this.DelegatedProcess = delegatedProcess;
+        }
+
+        #endregion Constructor(s)
+
+        protected override void Process(T input) =>
+            this.DelegatedProcess(input);
+    }
+
+    public abstract class DelegatedProcessor<TInput, TOutput>
+        : Processor<TInput, TOutput>
+    {
+        public delegate TOutput ProcessDelegate(TInput input);
+
+        public ProcessDelegate DelegatedProcess { get; }
+
+        #region Constructor(s)
+
+        public DelegatedProcessor(ProcessDelegate delegatedProcess)
+        {
+            this.DelegatedProcess = delegatedProcess;
+        }
+
+        #endregion Constructor(s)
+
+        protected override TOutput Process(TInput input) =>
+            this.DelegatedProcess(input);
+    }
+}
