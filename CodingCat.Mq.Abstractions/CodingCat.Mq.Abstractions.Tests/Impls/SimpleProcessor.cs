1e3e2557b051fccbeff5334bff777316b22d2041
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-02-28 20:42:27 +0800

    added test cases and a simple processor for the abstract processor

---
+++ CodingCat.Mq.Abstractions/CodingCat.Mq.Abstractions.Tests/Impls/SimpleProcessor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CodingCat.Mq.Abstractions.Tests.Impls
+{
+    public class SimpleProcessor<T> : Processor<T>
+    {
+        public delegate void HandleInput(T input);
+        public delegate void HandleProcessException(Exception ex);
+
+        public HandleInput InputHandler { get; }
+        public HandleProcessException ProcessExceptionHandler { get; }
+
+        #region Constructor(s)
+
+        public SimpleProcessor(
+            HandleInput inputHandler,
+            HandleProcessException processExceptionHandler = null
+        )
+        {
+            this.InputHandler = inputHandler;
+            this.ProcessExceptionHandler = processExceptionHandler;
+        }
+
+        #endregion Constructor(s)
+
+        public override void OnProcessError(Exception ex)
+        {
+            this.ProcessExceptionHandler?.Invoke(ex);
+            base.OnProcessError(ex);
+        }
+
+        protected override void OnInput(T input) => this.InputHandler(input);
+    }
+}
---
+++ CodingCat.Mq.Abstractions/CodingCat.Mq.Abstractions.Tests/TestProcessors.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using CodingCat.Mq.Abstractions.Tests.Impls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingCat.Mq.Abstractions.Tests
+{
+    [TestClass]
+    public class TestProcessors
+    {
+        [TestMethod]
+        public void Test_ProcessInput_Ok()
+        {
+            // Arrange
+            var expected = Guid.NewGuid().ToString();
+            var actual = null as string;
+
+            // Act
+            new SimpleProcessor<string>(value => actual = value)
+            {
+                Timeout = TimeSpan.FromMilliseconds(100)
+            }.Process(expected);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_ProcessInput_ExceptionHandled()
+        {
+            // Arrange
+            var expected = new Exception();
+            var actual = null as Exception;
+
+            // Act
+            new SimpleProcessor<string>(
+                value => throw expected,
+                ex => actual = ex
+            ).Process(null);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_ProcessInput_Timeout()
+        {
+            // Arrange
+            var expected = null as string;
+            var actual = expected;
+
+            // Act
+            new SimpleProcessor<string>(
+                value =>
+                {
+                    Thread.Sleep(1500);
+                    actual = Guid.NewGuid().ToString();
+                }
+            )
+            {
+                Timeout = TimeSpan.FromMilliseconds(100)
+            }.Process(null);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
