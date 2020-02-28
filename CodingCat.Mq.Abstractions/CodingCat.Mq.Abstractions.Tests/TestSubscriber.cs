01507bc9ac59e95d4ab7a1ebed5b20afad3df420
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-02-28 23:35:58 +0800

    performed code maid

--- CodingCat.Mq.Abstractions/CodingCat.Mq.Abstractions.Tests/TestSubscriber.cs
+++ CodingCat.Mq.Abstractions/CodingCat.Mq.Abstractions.Tests/TestSubscriber.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using CodingCat.Mq.Abstractions.Tests.Impls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using CodingCat.Mq.Abstractions.Tests.Impls;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CodingCat.Mq.Abstractions.Tests
 {
