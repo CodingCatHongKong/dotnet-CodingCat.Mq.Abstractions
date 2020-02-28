ba48c835866a2ed2d7df820479738a36977cc898
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-02-28 22:31:06 +0800

    added ISubscriber

---
+++ CodingCat.Mq.Abstractions/CodingCat.Mq.Abstractions/Interfaces/ISubscriber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CodingCat.Mq.Abstractions.Interfaces
+{
+    public interface ISubscriber : IDisposable
+    {
+        event EventHandler Subscribed;
+
+        event EventHandler Received;
+
+        event EventHandler Completed;
+
+        event EventHandler Disposing;
+
+        event EventHandler Disposed;
+
+        ISubscriber Subscribe();
+    }
+}
