cb7989826cdb1a24eaa99bc5c7b45231ea6160a8
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-03-01 21:06:10 +0800

    added IConnectConfiguration to provide connect retry config

---
+++ CodingCat.Mq.Abstractions/CodingCat.Mq.Abstractions/Interfaces/IConnectConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CodingCat.Mq.Abstractions.Interfaces
+{
+    public interface IConnectConfiguration
+    {
+        TimeSpan TimeoutPerTry { get; }
+        TimeSpan RetryInterval { get; }
+        uint RetryUpTo { get; }
+    }
+}
